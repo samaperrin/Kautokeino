7f33180ceb25b81ecb1b12cfb45be19a207b53e2
samaperrin <sam.perrin@ntnu.no>
2017-09-21 10:11:12 +0200

    "deleted connecitivyt function and given own script in R folder"

--- dispersal_barriers.R
+++ dispersal_barriers.R
@@ -139,33 +139,3 @@ dim(final)
 connectivity_matrix  <- select(merge(final,connectivity,by = "ConID", all.x=TRUE), 2:4)
 head(connectivity_matrix)
 save(connectivity_matrix,file="./Data/connectivity_matrix.rda")
-
-
-
-
-##############################################
-#### Step 7: Add in fish absence/presence ----
-##############################################
-
-for (i in c("Burbot","Arcticcharr","Whitefish","Pike","Perch","Browntrout","Grayling"))
-{
-  step1 <- richness_all[,c(i,"locationID","eurolst_bio10")]                 # Isolate occurrence and location column
-  colnames(step1) <- c("occurrence", "locationID","temp")
-  step2 <- merge(step1,locationIDs,by="locationID",all.x=TRUE)              # Merge with lakeid
-  step3 <- step2[,c(2:4)]
-  
-  step4 <- connectivity_matrix %>%                                          # Whittle down to downstream leakes with
-    filter(lakeid %in% filter(step3,occurrence == "1")$waterBodyID,         # fish present
-           upstream_lake %in% filter(step3,occurrence != "NA")$waterBodyID)
-  step5  <- merge(step4,rename(step3,upstream_lake = waterBodyID),by="upstream_lake",all.x=TRUE)
-  # Introduce upstream lake
-  colnames(step5)  <- c("upstream_lake", "upstream_slope_max","downstream_lake","occurrenceStatus","temp")
-  step5 <- distinct(step5,upstream_lake,upstream_slope_max,downstream_lake,occurrenceStatus,temp)
-  # Get rid of any rows we have conflicting                                                                             # duplicates for
-  step6 <- merge(step5,tally(group_by(step5,upstream_lake,downstream_lake)),
-                 by=c("upstream_lake","downstream_lake"),all.x=TRUE)
-  step6$occurs  <- ifelse(step6$occurrenceStatus == "1",1,ifelse(step6$n > 1,1,0))
-  step6 <- distinct(step6,upstream_lake,upstream_slope_max,downstream_lake,occurs,temp)
-  assign(paste(strtrim(i,4),"D",sep=""),step6)
-}
-
